fa8a1830ef3ba7949cbe3576aacad3b8344de1b3
NicolasSeguro <nicolas.seguro@gmail.com>
2023-08-22 14:22:01 -0300

    PPT y ajustes en como se muestra el contenido

--- main.js
+++ main.js
@@ -1,27 +1,65 @@
+// var modal = document.getElementById("myModal");
+// var btn = document.getElementById("openModal");
+// var span = document.getElementById("closeModal");
+
+// btn.onclick = function() {
+//   console.log('pruebas');
+//   modal.style.display = "block";
+//   modal.style.height = "100vh";
+//   modal.style.position = "relative";
+// }
+
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+// // Cerrar el modal con la tecla "Esc"
+// document.addEventListener('keydown', function(event) {
+//   if (event.key === "Escape" && modal.style.display == "block") {  
+//     modal.style.display = "none";
+//   }
+// });
+
 var modal = document.getElementById("myModal");
-var btn = document.getElementById("openModal");
+var btns = document.querySelectorAll(".openModal"); // Obtiene todos los botones con la clase "openModal"
 var span = document.getElementById("closeModal");
+var iframe = document.getElementById("iframeModal");
 
-btn.onclick = function() {
-  console.log('pruebas');
-  modal.style.display = "block";
-  modal.style.height = "100vh";
-  modal.style.position = "relative";
-}
+// Abre el modal y establece el contenido del iframe
+btns.forEach(function(btn) {
+  btn.onclick = function() {
+    var link = this.getAttribute('data-link'); // Obtiene el atributo data-link del botón
+    iframe.src = link; // Establece la URL del iframe
+    modal.style.display = "block";
+    modal.style.height = "100vh";
+    modal.style.position = "relative";
+  };
+});
 
+// Cierra el modal
 span.onclick = function() {
   modal.style.display = "none";
+  iframe.src = ""; // Limpiar el contenido del iframe
 }
 
+// Cierra el modal si se hace clic fuera de él
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    iframe.src = ""; // Limpiar el contenido del iframe
   }
 }
 
-// Cerrar el modal con la tecla "Esc"
+// Cierra el modal con la tecla "Esc"
 document.addEventListener('keydown', function(event) {
-  if (event.key === "Escape" && modal.style.display == "block") {  
+  if (event.key === "Escape" && modal.style.display == "block") {
     modal.style.display = "none";
+    iframe.src = ""; // Limpiar el contenido del iframe
   }
 });
