7f13b7352fdfd1aff4badd5c18c5ac74b3391f3b
Augusto Rossetti Lorea <augustorossettilorea@gmail.com>
2023-08-30 17:06:46 -0300

    Corrijo posicion del modal, en funcion de donde se abre

--- main.js
+++ main.js
@@ -32,14 +32,26 @@ var span = document.getElementById("closeModal");
 var iframe = document.getElementById("iframeModal");
 
 // Abre el modal y establece el contenido del iframe
-btns.forEach(function(btn) {
-  btn.onclick = function() {
+btns.forEach(function (btn) {
+  btn.addEventListener('click', function () {
     var link = this.getAttribute('data-link'); // Obtiene el atributo data-link del botón
     iframe.src = link; // Establece la URL del iframe
+
+    // Obtiene la posición del botón
+    const rect = btn.getBoundingClientRect();
+    const top = rect.top + window.scrollY;
+    const left = rect.left + window.scrollX;
+
+    // Establece la posición del modal encima del botón
     modal.style.display = "block";
+    modal.style.top = top + "px";
+    modal.style.left = "20%";
     modal.style.height = "100vh";
-    modal.style.position = "relative";
-  };
+    modal.style.width = "auto";
+    const ancho = modal.offsetWidth;
+    console.log("Ancho del div:", ancho, "px");
+    // modal.style.position = "absolute";
+  });
 });
 
 // Cierra el modal
